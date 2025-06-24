b601f422fe42e8e97f7144c5ad963fcbe4ec738a
apansky <apansky@gmail.com>
2025-06-24 21:27:38 +0200

    feat: initial data

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -1,37 +1,34 @@
-import { IMenuItem, ISocials } from "@/types";
+import { IMenuItem, ISocials } from '@/types';
 
 export const footerDetails: {
-    subheading: string;
-    quickLinks: IMenuItem[];
-    email: string;
-    telephone: string;
-    socials: ISocials;
+  subheading: string;
+  quickLinks: IMenuItem[];
+  email: string;
+  telephone: string;
+  socials: ISocials;
 } = {
-    subheading: "Empowering businesses with cutting-edge financial technology solutions.",
-    quickLinks: [
-        {
-            text: "Features",
-            url: "#features"
-        },
-        {
-            text: "Pricing",
-            url: "#pricing"
-        },
-        {
-            text: "Testimonials",
-            url: "#testimonials"
-        }
-    ],
-    email: 'address@yoursite.com',
-    telephone: '+1 (123) 456-7890',
-    socials: {
-        // github: 'https://github.com',
-        // x: 'https://twitter.com/x',
-        twitter: 'https://twitter.com/Twitter',
-        facebook: 'https://facebook.com',
-        // youtube: 'https://youtube.com',
-        linkedin: 'https://www.linkedin.com',
-        // threads: 'https://www.threads.net',
-        instagram: 'https://www.instagram.com',
+  subheading:
+    'Finančnému sprostredkovaniu sa venujeme už od roku 2010 a vlastná značka je prirodzeným vyústením našej kvalitnej práce. Sme lokálna značka, ktorá stavia na tom, že naše mesto nie je veľké a dobré i zlé referencie sa šíria rýchlosťou blesku. My Vám garantujeme 100% odbornosť, osobný prístup a spokojnosť. Stojíme si za našou prácou a sme v nej fakt dobrí.',
+  quickLinks: [
+    {
+      text: 'Features',
+      url: '#features'
+    },
+    {
+      text: 'Pricing',
+      url: '#pricing'
+    },
+    {
+      text: 'Testimonials',
+      url: '#testimonials'
     }
-}
+  ],
+  email: 'address@yoursite.com',
+  telephone: '+1 (123) 456-7890',
+  socials: {
+    twitter: 'https://twitter.com/Twitter',
+    facebook: 'https://facebook.com',
+    linkedin: 'https://www.linkedin.com',
+    instagram: 'https://www.instagram.com'
+  }
+};
--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,12 +1,13 @@
 export const siteDetails = {
-    siteName: 'Finwise',
-    siteUrl: 'https://finwise-omega.vercel.app/',
-    metadata: {
-        title: 'Finwise - Next.js and Tailwind CSS Landing Page Template',
-        description: 'Finwise empowers businesses with cutting-edge technology solutions to drive success and efficiency.',
-    },
-    language: 'en-us',
-    locale: 'en-US',
-    siteLogo: `${process.env.BASE_PATH || ''}/images/logo.png`, // or use a string for the logo e.g. "TechStartup"
-    googleAnalyticsId: '', // e.g. G-XXXXXXX,
-}
+  siteName: 'FinEQ',
+  siteUrl: 'https://finwise-omega.vercel.app/',
+  metadata: {
+    title: 'FinEQ - Prvý dokonalý finančník',
+    description:
+      'Finančnému sprostredkovaniu sa venujeme už od roku 2010 a vlastná značka je prirodzeným vyústením našej kvalitnej práce. Sme lokálna značka, ktorá stavia na tom, že naše mesto nie je veľké a dobré i zlé referencie sa šíria rýchlosťou blesku. My Vám garantujeme 100% odbornosť, osobný prístup a spokojnosť. Stojíme si za našou prácou a sme v nej fakt dobrí.'
+  },
+  language: 'sk-SK',
+  locale: 'sk-SK',
+  siteLogo: `${process.env.BASE_PATH || ''}/images/logo.png`, // or use a string for the logo e.g. "TechStartup"
+  googleAnalyticsId: '' // e.g. G-XXXXXXX,
+};
