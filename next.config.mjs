945efbb22d6f30356d933cba3fcd9a31ea86c63f
apansky <apansky@gmail.com>
2025-06-24 23:24:36 +0200

    chore: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`
+};
 
 export default nextConfig;
