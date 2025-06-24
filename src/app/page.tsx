01abc95fc4ff52d29a4a1b93134c77e848345f7c
apansky <apansky@gmail.com>
2025-06-24 23:18:24 +0200

    fix: eslint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Hero from "@/components/Hero";
-import Testimonials from "@/components/Testimonials";
-import Pricing from "@/components/Pricing/Pricing";
+// import Testimonials from "@/components/Testimonials";
+// import Pricing from "@/components/Pricing/Pricing";
 import Benefits from "@/components/Benefits/Benefits";
 import Container from "@/components/Container";
-import Section from "@/components/Section";
-import Stats from "@/components/Stats";
+// import Section from "@/components/Section";
+// import Stats from "@/components/Stats";
 
 const HomePage: React.FC = () => {
   return (
